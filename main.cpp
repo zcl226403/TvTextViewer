ccf181f5736235f279cdce73fb5f1163beadcd8f
zcl226403 <837168573@qq.com>
2023-01-02 17:45:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -349,7 +349,7 @@ int main(int argc, char** argv)
   {
     ImFontConfig config;
     config.SizePixels = args["font_size"].as<int>();
-    ImGui::GetIO().Fonts->AddFontFromFileTTF("c:/windows/fonts/simhei.ttf", &config, NULL, ImGui::GetIO().fonts->GetGlyphRangesChineseSimplifiedCommon());
+    ImGui::GetIO().Fonts->AddFontFromFileTTF("/storage/.config/retroarch/regular.ttf", &config, NULL, ImGui::GetIO().fonts->GetGlyphRangesChineseSimplifiedCommon());
   }
 
   // Setup Platform/Renderer bindings
